77b7c72d9313c99d7b4f390b97505256bcb984e5
Bo0tsickle <105829802+Bo0tsickle@users.noreply.github.com>
2023-03-27 13:20:39 -0700

    update the numbers

--- fibonacci1.cs
+++ fibonacci1.cs
@@ -1,6 +1,7 @@
 using System;
 
 int fib1 = 1;
+int tempFib = 0;
 int fib2 = 2;
 int sum = 2;
 
@@ -8,6 +9,9 @@ for (int i = 0; i < 4_000_000; i++) {
     if ((fib1 + fib2 == i) && (i % 2 == 0)) {
         sum = sum + i;
     }
+    tempFib = fib1 + fib2;
+    fib1 = fib2;
+    fib2 = tempFib;
 }
 
 Console.WriteLine(sum);
