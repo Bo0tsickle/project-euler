a25558df23ddd33e420436887641c7cdddfcbd9c
Bo0tsickle <105829802+Bo0tsickle@users.noreply.github.com>
2023-03-27 13:13:58 -0700

    made the program time to debug

--- fibonacci1.cs
+++ fibonacci1.cs
@@ -1,1 +1,13 @@
 using System;
+
+int fib1 = 1;
+int fib2 = 2;
+int sum = 2;
+
+for (int i = 0; i < 4_000_000; i++) {
+    if ((fib1 + fib2 == i) && (i % 2 == 0)) {
+        sum += i;
+    }
+}
+
+return sum;
